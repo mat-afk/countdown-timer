6827c8e5811ada8198c137aa9295c5274cdcfd46
Mateus <mateusc.ramos@outlook.com>
2024-02-03 17:38:18 -0300

    refactor: countdown display in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import "./App.css";
+import useCountdown from "./hooks/useCountdown";
+
 import Counter from "./components/Counter";
 import Title from "./components/Title";
 
+import "./App.css";
+
 function App() {
+  const {days, hours, minutes, seconds} = useCountdown("04/01/2024 00:00:00");
+
   return (
     <div className="App" style={{ background: "#080404" }}>
       <div className="container">
         <Title title={"Countdown to 2024 New Year's Day"} />
         <div className="countdown-container">
-          <Counter text={"Days"} number={2} />
-          <Counter text={"Hours"} number={2} />
-          <Counter text={"Minutes"} number={2} />
-          <Counter text={"Seconds"} number={2} />
+          <Counter text={"Days"} number={days} />
+          <Counter text={"Hours"} number={hours} />
+          <Counter text={"Minutes"} number={minutes} />
+          <Counter text={"Seconds"} number={seconds} />
         </div>
       </div>
     </div>
