3ce7885bf9961b3925986d1000c366112b42a4ab
Mateus <mateusc.ramos@outlook.com>
2024-02-03 23:57:57 -0300

    feat: form listeners

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,56 @@
+import { useState } from "react";
+
 import "./Home.css";
+
 const Home = () => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [image, setImage] = useState("");
+  const [color, setColor] = useState("#000000");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const countdownConfig = {
+      title,
+      date,
+      image,
+      color,
+    };
+
+    console.log(countdownConfig);
+  };
+
   return (
     <div className="home">
       <h2>Make your personalized Countdown!</h2>
-      <form className="countdown-form">
+      <form className="countdown-form" onSubmit={handleSubmit}>
         <label>
-          <span>Title</span>
+          <span>
+            Title <span className="required">*</span>
+          </span>
           <input
             type="text"
             name="title"
             id="title"
             placeholder="Enter the name of the event"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
           />
         </label>
         <label>
-          <span>Date</span>
-          <input type="date" name="date" id="date" />
+          <span>
+            Date <span className="required">*</span>
+          </span>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+            required
+          />
         </label>
         <label>
           <span>Background Image</span>
@@ -24,11 +59,19 @@ const Home = () => {
             name="image"
             id="image"
             placeholder="Enter the image's URL"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
           />
         </label>
         <label>
           <span>Color</span>
-          <input type="color" name="color" id="color" />
+          <input
+            type="color"
+            name="color"
+            id="color"
+            onChange={(e) => setColor(e.target.value)}
+            value={color}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
